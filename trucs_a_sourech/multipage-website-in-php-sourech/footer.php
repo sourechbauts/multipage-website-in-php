6e2d50cfcf121439b01da8715d81f6be887fe08a
Your Name <you@example.com>
2018-08-08 13:26:13 +0200

    page entreprise quasi ok

---
+++ trucs_a_sourech/multipage-website-in-php-sourech/footer.php
@@ -0,0 +1,44 @@
+<footer>
+  <div id="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3 text-center">
+          <p class="fh5co-social-icons">
+            <a href="https://www.linkedin.com/company/stichting-onze-kleine-weeskinderen-fondation-nos-petits-orphelins/ target="_blank""><i class="icon-linkedin2"></i></a>
+            <a href="https://www.facebook.com/stichtingonzekleineweeskinderen" target="_blank"><i class="icon-facebook2"></i></a>
+            <a href="https://www.youtube.com/user/nphbelgium target='_blank'"><i class="icon-youtube"></i></a>
+          </p>
+          <p>Copyright 2016 Free Html5 <a href="#">Charity</a>. All Rights Reserved. <br>Made with <i class="icon-heart3"></i> by <a href="http://freehtml5.co/" target="_blank">Freehtml5.co</a> / Demo Images: <a href="https://unsplash.com/" target="_blank">Unsplash</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+</div>
+<!-- END fh5co-page -->
+
+</div>
+<!-- END fh5co-wrapper -->
+
+<!-- jQuery -->
+
+
+<script src="js/jquery.min.js"></script>
+<!-- jQuery Easing -->
+<script src="js/jquery.easing.1.3.js"></script>
+<!-- Bootstrap -->
+<script src="js/bootstrap.min.js"></script>
+<!-- Waypoints -->
+<script src="js/jquery.waypoints.min.js"></script>
+<script src="js/sticky.js"></script>
+
+<!-- Stellar -->
+<script src="js/jquery.stellar.min.js"></script>
+<!-- Superfish -->
+<script src="js/hoverIntent.js"></script>
+<script src="js/superfish.js"></script>
+
+<!-- Main JS -->
+<script src="js/main.js"></script>
